73c7e15d148242d84abe24d16faef0b8604a416e
EGAguirre <9eaguirre@stpatrick.org>
2017-04-21 10:54:21 -0500

    Add files via upload

--- Hover.js
+++ Hover.js
@@ -1,2 +1,15 @@
 (function(){
   const doc = document;
+  let aricle img = doc.querySelector("div")
+
+  article img.addEventListener("click",function(){
+    let imgclass = article img.className;
+    if(imgclass == "circle"){
+      article img.className = "rectangle";
+
+    }
+    else{
+      article img.className = "circle";
+
+    }
+  });
